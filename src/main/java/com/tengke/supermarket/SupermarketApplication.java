1745679b21f663cba5f296547f4a6e6442546f92
xizhizai <416595479@qq.com>
2020-07-23 12:38:52 +0800

    创建项目

---
+++ src/main/java/com/tengke/supermarket/SupermarketApplication.java
@@ -0,0 +1,13 @@
+package com.tengke.supermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupermarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tengke/supermarket/SupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tengke.supermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
