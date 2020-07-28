b856e37465a32751b80431084e1781c08d6f9c06
SssS <543922673@qq.com>
2020-07-28 21:59:01 +0800

    修改日期格式

--- src/main/java/com/tengke/supermarket/model/SellRecord.java
+++ src/main/java/com/tengke/supermarket/model/SellRecord.java
@@ -1,6 +1,7 @@
 package com.tengke.supermarket.model;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -37,6 +38,7 @@ public class SellRecord {
      * 销售日期
      */
     @NonNull
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date sellDate;
 
 
